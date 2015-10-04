7b76deb35476ef401eeedc6d0de112409d47f455
evanl81 <lambert.evan@gmail.com>
2015-10-04 12:45:11 -0400

    Updated with feedback incorporated.

--- app/src/main/java/net/evanlambert/capstone/MainActivity.java
+++ app/src/main/java/net/evanlambert/capstone/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     private Button buttonBuild;
     private Button buttonxyz;
     private Button buttonCapstone;
+    private Toast mAppToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,70 +25,46 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     public void buttonOnClick(View view) {
 
         Context context = getApplicationContext();
         int duration = Toast.LENGTH_SHORT;
-        CharSequence text;
-        Toast toast;
+        String appName = ((Button)view).getText().toString();
+        CharSequence text = "This is where " + appName + "\nwill launch.";
+
+        if(mAppToast !=null) {
+            mAppToast.cancel();
+        }
 
         switch(view.getId()) {
             case R.id.spotifyButton:
-                text = "This is where my Spotify app will launch.";
-                toast = Toast.makeText(context, text, duration);
-                toast.show();
+                mAppToast = Toast.makeText(context, text, duration);
+                mAppToast.show();
                 break;
 
             case R.id.scoresButton:
-                text = "This is where my Scores app will launch.";
-                toast = Toast.makeText(context, text, duration);
-                toast.show();
+                mAppToast = Toast.makeText(context, text, duration);
+                mAppToast.show();
                 break;
 
             case R.id.libraryButton:
-                text = "This is where my Library app will launch.";
-                toast = Toast.makeText(context, text, duration);
-                toast.show();
+                mAppToast = Toast.makeText(context, text, duration);
+                mAppToast.show();
                 break;
 
             case R.id.biggerButton:
-                text = "This is where my Build It Bigger\napp will launch.";
-                toast = Toast.makeText(context, text, duration);
-                toast.show();
+                mAppToast = Toast.makeText(context, text, duration);
+                mAppToast.show();
                 break;
 
             case R.id.xyzButton:
-                text = "This is where my XYZ Reader \napp will launch.";
-                toast = Toast.makeText(context, text, duration);
-                toast.show();
+                mAppToast = Toast.makeText(context, text, duration);
+                mAppToast.show();
                 break;
 
             case R.id.capstoneButton:
-                text = "This is where my Capstone\napp will launch.";
-                toast = Toast.makeText(context, text, duration);
-                toast.show();
+                mAppToast = Toast.makeText(context, text, duration);
+                mAppToast.show();
                 break;
 
 
